d9dd1f811bae80e9c432b78d541a8718367388a0
GoldentTuft <goldent@outlook.jp>
2020-05-02 02:50:58 +0900

    uuu

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Dom
 import Html exposing (Html)
 import Html.Attributes
 import Html.Attributes.Classname exposing (classMixinWith)
@@ -11,6 +12,7 @@ import Neat exposing (NoGap, Protected, Renderer, View, defaultRenderer, fromNoG
 import Neat.Layout as Layout
 import Neat.Layout.Column as Column exposing (Column, defaultColumn)
 import Neat.Layout.Row as Row exposing (defaultRow)
+import Task
 
 
 
@@ -29,12 +31,18 @@ main =
 
 
 type alias Model =
-    { dropDown1Open : Bool }
+    { dropDown1Open : Bool
+    , modalOpen : Bool
+    , hoverOpen : Bool
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { dropDown1Open = False }
+    ( { dropDown1Open = False
+      , modalOpen = False
+      , hoverOpen = False
+      }
     , Cmd.none
     )
 
@@ -42,6 +50,11 @@ init =
 type Msg
     = NoOp
     | ToggleDropDown1
+    | OpenDropDown1
+    | ToggleModal
+    | SelectItem String
+    | OpenHover
+    | CloseHover
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -52,6 +65,25 @@ update msg model =
 
         ToggleDropDown1 ->
             ( { model | dropDown1Open = not model.dropDown1Open }, Cmd.none )
+
+        OpenDropDown1 ->
+            ( { model | dropDown1Open = True }, Cmd.none )
+
+        ToggleModal ->
+            ( { model | modalOpen = not model.modalOpen }, Cmd.none )
+
+        SelectItem str ->
+            ( { model | modalOpen = False, hoverOpen = False }, Cmd.none )
+
+        OpenHover ->
+            ( { model | hoverOpen = True }, Cmd.none )
+
+        CloseHover ->
+            ( { model | hoverOpen = False }, Cmd.none )
+
+
+
+--Task.attempt (\_ -> NoOp) (Browser.Dom.focus "Brand") )
 
 
 type MenuGap
@@ -78,9 +110,120 @@ modal alpha =
         |> setStyle "z-index" "1000"
         |> setStyle "top" "0"
         |> setStyle "left" "0"
-        |> setStyle "width" "100%"
-        |> setStyle "height" "100%"
+        |> setStyle "width" "100vw"
+        |> setStyle "height" "100vh"
         |> setStyle "cursor" "default"
+        |> setAttribute (Html.Events.onClick ToggleModal)
+
+
+hoverMenuItem : String -> View NoGap Msg
+hoverMenuItem label =
+    Neat.textBlock label
+        |> setAttribute (Html.Events.onMouseDown (SelectItem label))
+        |> setAttribute (Html.Attributes.tabindex -1)
+        |> setClass "dropDown_item"
+
+
+hoverMenu1 : Column.Horizontal -> String -> Bool -> View NoGap Msg
+hoverMenu1 dir1 dir2 open =
+    Layout.column
+        [ Neat.textBlock "hoverMenu1"
+        , Layout.columnWith { defaultColumn | horizontal = dir1 }
+            [ Layout.column
+                [ hoverMenuItem "hoverItem1"
+                , hoverMenuItem "hoverItem2aaaaaaaaaaaaaaa"
+                ]
+            ]
+            |> setClass "dropDown_items"
+            |> setClass dir2
+            |> Neat.when open
+        ]
+        |> setClass "dropDown"
+        |> setAttribute (Html.Events.onMouseEnter OpenHover)
+        |> setAttribute (Html.Events.onMouseLeave CloseHover)
+
+
+hoverMenu2 : Column.Horizontal -> String -> Bool -> View NoGap Msg
+hoverMenu2 dir1 dir2 open =
+    Layout.column
+        [ Neat.textBlock "hoverMenu2"
+        , Layout.columnWith { defaultColumn | horizontal = dir1 }
+            [ Layout.column
+                [ hoverMenuItem "hoverItem1"
+                , hoverMenuItem "hoverItem2aaaaaaaaaaaaaaa"
+                ]
+            ]
+            |> setClass "dropDown_items"
+            |> setClass dir2
+            |> Neat.when open
+        ]
+        |> setClass "dropDown"
+        |> setAttribute (Html.Events.onMouseEnter OpenHover)
+        |> setAttribute (Html.Events.onMouseLeave CloseHover)
+
+
+pon : Bool -> (View NoGap Msg -> View NoGap Msg) -> (View NoGap Msg -> View NoGap Msg)
+pon flag v =
+    \a ->
+        if flag then
+            a |> v
+
+        else
+            a
+
+
+dropDownItem : String -> View NoGap Msg
+dropDownItem label =
+    Neat.textBlock label
+        |> setAttribute (Html.Events.onMouseDown (SelectItem label))
+        |> setAttribute (Html.Attributes.tabindex -1)
+        |> setClass "dropDownToggle_item"
+
+
+dropDown1 : Model -> View NoGap Msg
+dropDown1 model =
+    Layout.column
+        [ Layout.column
+            [ Neat.textBlock "dropDownMenu1"
+            , Layout.columnWith
+                { defaultColumn | horizontal = Column.Left }
+                [ Layout.column
+                    [ dropDownItem "menu5"
+                    , dropDownItem "menu22222222222222222"
+                    , hoverMenu1 Column.Left "childRight" model.hoverOpen
+                    ]
+                    |> setClass "dropDownToggle_items"
+                ]
+            ]
+            |> pon (not model.modalOpen) (setAttribute (Html.Events.onMouseDown ToggleModal))
+            |> setAttribute (Html.Attributes.tabindex -1)
+            |> setClass "dropDownToggle"
+        , modal 0.5
+            |> Neat.when model.modalOpen
+        ]
+
+
+dropDown2 : Model -> View NoGap Msg
+dropDown2 model =
+    Layout.column
+        [ Layout.column
+            [ Neat.textBlock "dropDownMenu2"
+            , Layout.columnWith
+                { defaultColumn | horizontal = Column.Right }
+                [ Layout.column
+                    [ dropDownItem "menu5"
+                    , dropDownItem "menu22222222222222222"
+                    , hoverMenu1 Column.Right "childLeft" model.hoverOpen
+                    ]
+                    |> setClass "dropDownToggle_items"
+                ]
+            ]
+            |> pon (not model.modalOpen) (setAttribute (Html.Events.onMouseDown ToggleModal))
+            |> setAttribute (Html.Attributes.tabindex -1)
+            |> setClass "dropDownToggle"
+        , modal 0.5
+            |> Neat.when model.modalOpen
+        ]
 
 
 menu : Model -> View NoGap Msg
@@ -90,69 +233,16 @@ menu model =
             |> setClass "menu_brand"
             |> fromNoGap menuGap
         , Layout.rowWith { defaultRow | horizontal = Row.Right, vertical = Row.VCenter, wrap = Row.Wrap }
-            [ menuItem "Item2"
-            , Layout.column
-                [ Neat.textBlock "Item444444"
-                , if model.dropDown1Open then
-                    Layout.columnWith { defaultColumn | horizontal = Column.Right }
-                        [ Layout.column
-                            [ --Neat.empty
-                              --|> setClass "modal"
-                              Neat.textBlock "menu1"
-                                |> setAttribute (Html.Events.onClick NoOp)
-                                |> setClass "dropDownToggle_item"
-                            , Neat.textBlock "menu22222222222222222"
-                                |> setClass "dropDownToggle_item"
-                            , Layout.column
-                                [ Neat.textBlock "Item444444"
-                                , Layout.columnWith { defaultColumn | horizontal = Column.Right }
-                                    [ Layout.column
-                                        [ --Neat.empty
-                                          -- |> setClass "modal"
-                                          Neat.textBlock "menu1"
-                                            |> setClass "dropDown_item"
-                                        , Neat.textBlock "menu22222222222222222"
-                                            |> setClass "dropDown_item"
-                                        ]
-                                    ]
-                                    |> setClass "dropDown_items"
-                                    |> setClass "childRight"
-                                ]
-                                |> setClass "dropDown"
-                                |> setClass "dropDown_item"
-                            , modal 0
-
-                            -- Neat.empty
-                            --     |> setClass "modal"
-                            ]
-                        ]
-                        |> setClass "dropDownToggle_items"
-
-                  else
-                    Neat.none
-                ]
-                -- |> setAttribute (Html.Attributes.tabindex 0)
-                |> setAttribute (Html.Events.onClick ToggleDropDown1)
-                |> setClass "dropDownToggle"
+            [ dropDown1 model
                 |> fromNoGap menuGap
+            , hoverMenu1 Column.Left "childBottom" model.hoverOpen
+                |> fromNoGap menuGap
+            , menuItem "Item2"
             , menuItem "Item3"
-            , Layout.column
-                [ Neat.textBlock "Item444444"
-                , Layout.columnWith { defaultColumn | horizontal = Column.Right }
-                    [ Layout.column
-                        [ --Neat.empty
-                          -- |> setClass "modal"
-                          Neat.textBlock "menu1"
-                            |> setClass "dropDown_item"
-                        , Neat.textBlock "menu22222222222222222"
-                            |> setClass "dropDown_item"
-                        ]
-                    ]
-                    |> setClass "dropDown_items"
-                ]
-                |> setClass "dropDown"
+            , dropDown2 model
                 |> fromNoGap menuGap
-            , menuItem "Item5"
+            , hoverMenu2 Column.Right "childBottom" model.hoverOpen
+                |> fromNoGap menuGap
             ]
             |> setLayout Layout.fill
         ]
@@ -190,25 +280,23 @@ box =
 scrollAreaY : List (View NoGap msg) -> View NoGap msg
 scrollAreaY contents =
     Layout.column
-        [ Layout.rowWith { defaultRow | vertical = Row.Stretch }
-            [ Layout.column
-                contents
-                |> setStyle "position" "absolute"
-                |> setStyle "background-color" "rgb(0, 100, 100)"
-                |> setStyle "height" "100%"
-                |> setStyle "overflow-y" "auto"
-            ]
-            |> setStyle "position" "relative"
+        [ Layout.column
+            contents
+            |> setStyle "position" "absolute"
+            |> setStyle "height" "100%"
+            -- |> setStyle "height" "500px"
+            |> setStyle "overflow-y" "auto"
         ]
+        |> setStyle "position" "relative"
 
 
 body : View NoGap msg
 body =
-    Layout.rowWith { defaultRow | wrap = Row.Wrap, vertical = Row.Top }
-        [ Neat.textBlock "leftSide"
+    Layout.rowWith { defaultRow | wrap = Row.Wrap }
+        --, vertical = Row.Top }
+        [ Layout.column [ Neat.textBlock "leftSide" ]
             |> fromNoGap bodyGap
             |> setBoundary bodyGap
-            |> setLayout (Layout.fillBy 15)
             |> setClass "sidebar"
         , scrollAreaY
             [ Neat.textBlock "content"
@@ -219,13 +307,9 @@ body =
             |> setClass "content"
             |> fromNoGap bodyGap
             |> setBoundary bodyGap
-            |> setLayout (Layout.fillBy 70)
-        , Neat.textBlock "rightSide"
-            |> fromNoGap bodyGap
-            |> setBoundary bodyGap
-            |> setLayout (Layout.fillBy 15)
-            |> setClass "sidebar"
+            |> setLayout Layout.fill
         ]
+        |> setLayout (Layout.fillBy 80)
         |> setClass "body"
 
 
